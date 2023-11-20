e6d37fb570088427a6aae7872731ef2418c6f9b5
ilonas07 <sa1177@mci4me.at>
2023-11-20 22:05:33 +0100

    update 20112023_22:05

---
+++ framework/.ipynb_checkpoints/main_func-checkpoint.py
@@ -0,0 +1,58 @@
+import torch
+
+
+def main_train_func(input_args):
+    # print(input_args)
+    train_loader, val_loader, model, criterion, optimizer, exp_lr_scheduler, device, epoch = input_args
+
+    model.train()
+    running_loss = 0.0
+    running_corrects = 0
+
+    for inputs, labels in train_loader:
+        inputs = inputs.to(device)
+        labels = labels.to(device)
+
+        optimizer.zero_grad()
+        outputs = model(inputs)
+        _, preds = torch.max(outputs, 1)
+        loss = criterion(outputs, labels)
+
+        loss.backward()
+        optimizer.step()
+
+        running_loss += loss.item() / inputs.size(0)
+        running_corrects += torch.sum(preds == labels.data) / inputs.size(0)
+
+    exp_lr_scheduler.step()
+    train_epoch_loss = running_loss / len(train_loader)
+    train_epoch_acc = running_corrects / len(train_loader)
+
+    model.eval()
+    running_loss = 0.0
+    running_corrects = 0
+
+    for inputs, labels in (val_loader):
+        inputs = inputs.to(device)
+        labels = labels.to(device)
+        outputs = model(inputs)
+        _, preds = torch.max(outputs, 1)
+        loss = criterion(outputs, labels)
+
+        running_loss += loss.item() / inputs.size(0)
+        running_corrects += torch.sum(preds == labels.data) / inputs.size(0)
+
+    epoch_loss = running_loss / len(val_loader)
+    epoch_acc = running_corrects.double() / len(val_loader)
+    print(
+        f"Epoch: {epoch}: Train: Loss: {train_epoch_loss:.4f} Acc: {train_epoch_acc:.4f} Val: Loss: {epoch_loss:.4f} Acc: {epoch_acc:.4f}")
+
+    return_val = {}
+    return_val["loss"] = train_epoch_loss
+    return_val["accuracy"] = train_epoch_acc.item()
+    return_val["val_loss"] = epoch_loss
+    return_val["val_accuracy"] = epoch_acc.item()
+
+    print(return_val)
+
+    return return_val
